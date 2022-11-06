d0237a3eb55673bb1c36c646f4441a0ac4205e49
Kushal Bhayal <Kushalbhayal01@gmail.com>
2022-11-06 17:18:49 +0530

    First commit

---
+++ src/com/lokedme/MyFile.java
@@ -0,0 +1,31 @@
+ package com.lokedme;
+
+public class MyFile {
+
+private int id;
+private String fileName;
+
+public MyFile() {
+
+}
+public MyFile(int id, String fileName) {
+	super();
+	this.id = id;
+	this.fileName = fileName;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getFileName() {
+	return fileName;
+}
+public void setFileName(String fileName) {
+	this.fileName = fileName;
+}
+
+	
+
+}
